43810125d5d776ec57cd9b2c7cb04f1d54f1f4f0
notken12 <kendotzhou@gmail.com>
2024-07-13 19:26:29 -0400

    multi 3d card kinda working

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "**",
+      },
+    ],
+    domains: [
+      "api.microlink.io", // Microlink Image Preview
+    ],
+  },
+};
 
 export default nextConfig;
