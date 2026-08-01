688006feccb2fd03463c523343a098bebd9ca9f1
notken12 <kenzhou084@gmail.com>
2026-08-01 15:09:46 -0500

    remove some old stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,6 @@
 "use client";
 
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import Image from "next/image";
-import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-card";
 import Link from "next/link";
 import { LinkPreview } from "@/components/ui/link-preview";
 import { Badge } from "@/components/ui/badge";
@@ -43,98 +35,6 @@ export default function Home() {
               </LinkPreview>, the AI intelligence layer for logistics.
             </div>
           </section>
-
-          {/* <section className="w-full flex flex-col items-start mb-8">
-            <h2 className="scroll-m-20 text-xl font-semibold tracking-tight font-handwriting">
-              🛠️ Currently building...
-            </h2>
-            <div className="h-7"></div>
-            <CardContainer
-              containerClassName="flex flex-grow w-full"
-              className="flex flex-grow w-full"
-              rotateness={100}
-            >
-              <CardBody className="border border-neutral-300 dark:border-neutral-700 w-full aspect-[8/5] sm:aspect-[16/9] bg-muted dark:bg-[#303030] rounded-md shadow-xl relative flex flex-col items-center">
-                <div className="flex flex-col mb-4 sm:mb-8 flex-grow w-full">
-                  <div
-                    className="flex-grow relative flex"
-                    style={{ perspective: "1000px" }}
-                  >
-                    <CardItem
-                      as={"a"}
-                      className="flex flex-col bg-neutral-200 dark:bg-muted shadow-xl border border-neutral-350 dark:border-neutral-700 rounded-md h-fit self-start absolute top-[28%] left-[16%] w-[30%] -rotate-6"
-                      rotateZ={-6}
-                      translateZ={30}
-                      href="https://neurallab.vercel.app"
-                      target="_blank"
-                    >
-                      <div className="flex justify-start p-1 gap-[0.2rem] h-4 items-center relative">
-                        <div className="bg-red-500 rounded-full w-2 h-2"></div>
-                        <div className="bg-yellow-500 rounded-full w-2 h-2"></div>
-                        <div className="bg-green-500 rounded-full w-2 h-2"></div>
-                        <p className="text-xs text-muted-foreground font-handwriting absolute inset-0 text-center leading-[1.1rem] hidden sm:block">
-                          NeuralLab
-                        </p>
-                      </div>
-                      <img
-                        src="/images/neurallab.webp"
-                        alt="NeuralLab UI"
-                        fetchPriority="high"
-                        className="shadow-md w-full h-auto rounded-b-md dark:hidden"
-                      />
-                      <img
-                        src="/images/neurallab-dark.webp"
-                        alt="NeuralLab UI"
-                        fetchPriority="high"
-                        className="shadow-md w-full h-auto rounded-b-md hidden dark:block"
-                      />
-                    </CardItem>
-                    <CardItem
-                      className="flex flex-col bg-neutral-200 dark:bg-muted shadow-xl bg-border dark:border-muted border-8 outline outline-1 outline-neutral-350 dark:outline-neutral-700 rounded-md h-fit self-start absolute top-[24%] right-[17%] w-[20%] rotate-6"
-                      rotateZ={6}
-                      translateZ={30}
-                    >
-                      <img
-                        src="/images/usbstation.webp"
-                        alt="USB Charging Station"
-                        fetchPriority="high"
-                        className="shadow-md w-full h-auto rounded-b-md"
-                      />
-                      <p className="text-xs text-muted-foreground font-handwriting leading-none mt-1 mb-[-0.25rem]">
-                        USB Station
-                      </p>
-                    </CardItem>
-                  </div>
-                  <div className="flex flex-row gap-7 justify-center items-center">
-                    <img
-                      src="/images/keyboard.svg"
-                      alt="Keyboard"
-                      fetchPriority="high"
-                      className="h-10 sm:h-14  object-contain object-center shadow-md max-w-fit dark:hidden w-auto"
-                    />
-                    <img
-                      src="/images/keyboard-dark.svg"
-                      alt="Keyboard"
-                      fetchPriority="high"
-                      className="h-10 sm:h-14  object-contain object-center shadow-md max-w-fit hidden dark:block w-auto"
-                    />
-                    <img
-                      src="/images/pen.svg"
-                      alt="Pen"
-                      fetchPriority="high"
-                      className="h-12 sm:h-16 object-contain object-center max-w-fit dark:hidden w-auto"
-                    />
-                    <img
-                      src="/images/pen-dark.svg"
-                      alt="Pen"
-                      fetchPriority="high"
-                      className="h-12 sm:h-16 object-contain object-center max-w-fit hidden dark:block w-auto"
-                    />
-                  </div>
-                </div>
-              </CardBody>
-            </CardContainer>
-          </section> */}
 
           <section className="w-full flex flex-col items-start">
             <h2 className="scroll-m-20 text-xl font-semibold tracking-tight">
@@ -220,29 +120,6 @@ export default function Home() {
               <div className="flex flex-row sm:flex-col justify-between sm:justify-normal items-center gap-1 sm:items-end mb-4 sm:mb-0">
                 <LinkPreview
                   className="font-semibold text-primary underline underline-offset-4 leading-7"
-                  url="https://cider-app.vercel.app"
-                >
-                  Cider
-                </LinkPreview>
-                <Badge variant={"default"}>🏆 Best Lifestyle Hack</Badge>
-              </div>
-
-              <p className="leading-7 mb-6 sm:mb-0 sm:ml-4">
-                Add reminders, agendas, and events from documents into your
-                calendar automatically using AI.&nbsp;
-                <a
-                  className="text-muted-foreground text-sm underline underline-offset-4 text-nowrap"
-                  href="https://hacktj.org"
-                  target="_blank"
-                >
-                  <MapPin className="w-4 h-4 inline-flex align-text-top mr-1" />
-                  HackTJ 11.0
-                </a>
-              </p>
-
-              <div className="flex flex-row sm:flex-col justify-between sm:justify-normal items-center gap-1 sm:items-end mb-4 sm:mb-0">
-                <LinkPreview
-                  className="font-semibold text-primary underline underline-offset-4 leading-7"
                   url="https://crates.io/crates/licensesnip"
                 >
                   Licensesnip
@@ -251,44 +128,12 @@ export default function Home() {
               <p className="leading-7 mb-6 sm:mb-0 sm:ml-4">
                 Automatically add license headers to source code with just one
                 command.&nbsp;
-                <span className="font-mono font-semibold">20000+</span>
+                <span className="font-mono font-semibold">25000+</span>
                 &nbsp;downloads.
-              </p>
-
-              <div className="flex flex-row sm:flex-col justify-between sm:justify-normal items-center gap-1 sm:items-end mb-4 sm:mb-0">
-                <LinkPreview
-                  className="font-semibold text-primary underline underline-offset-4 leading-7 text-nowrap"
-                  url="https://github.com/notken12/mangochat"
-                >
-                  MangoChat
-                </LinkPreview>
-                <Badge variant={"default"}>
-                  <a
-                    href="https://devpost.com/software/mango-chat"
-                    target="_blank"
-                  >
-                    🏆 Best Overall Hack
-                  </a>
-                </Badge>
-              </div>
-              <p className="leading-7 mb-6 sm:mb-0 sm:ml-4">
-                Chat and send money securely using end-to-end encryption and
-                decentralized storage & currency.&nbsp;
-                <a
-                  className="text-muted-foreground text-sm underline underline-offset-4 text-nowrap"
-                  href="https://hacktj.org"
-                  target="_blank"
-                >
-                  <MapPin className="w-4 h-4 inline-flex align-text-top mr-1" />
-                  HackTJ 9.0
-                </a>
               </p>
             </div>
           </section>
           <section className="w-full flex flex-col items-center">
-            {/* <h2 className="scroll-m-20 text-muted-foreground font-semibold tracking-tight flex gap-4 items-center">
-              Contact me
-            </h2> */}
             <div className="grid grid-cols-1 sm:grid-cols-[auto_1fr] sm:gap-y-6 mt-6">
               <div className="flex flex-row gap-4 items-center">
                 <Mail className="size-5" />
